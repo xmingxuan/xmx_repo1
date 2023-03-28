98c231722bf0191fe97488e2f405fbb5528fa00c
xmx <xmmningxuan@dlmu.edu.cn>
2023-03-28 16:29:59 +0800

    加法

--- sum-sub.cpp
+++ sum-sub.cpp
@@ -8,21 +8,22 @@ return a + b;
 
 }
 
-int sub(int a, int b){
+/*int sub(int a, int b){
 
 if(a>b) return a-b;
 
 else return b-a; 
 
-}
+}*/
 int main(){
 
 int a, b, sum, dif;
 cout<<"请输入两个数：";
 cin>>a>>b;
 sum = add(a,b);
-dif = sub(a,b);
-cout<<"a+b="<<sum<<","<<"|a-b|="<<dif<<endl;
+//dif = sub(a,b);
+//cout<<"a+b="<<sum<<","<<"|a-b|="<<dif<<endl;
+cout<<"a+b"<<sum<<endl;
 return 0;
 
 }
